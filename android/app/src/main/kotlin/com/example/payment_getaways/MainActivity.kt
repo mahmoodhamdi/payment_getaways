8b6cfdf2ca5ff0b3ba37bc2deef9a29433d7201c
mahmoodhamdi <hmdy7486@gmail.com>
2024-08-27 00:37:12 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/payment_getaways/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.payment_getaways
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:payment_getaways/app.dart';
+
+void main() {
+  runApp(const App());
+}
