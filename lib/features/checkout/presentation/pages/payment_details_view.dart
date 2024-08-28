450363a8d075a49be4804ecbf7129a5bd30c6447
mahmoodhamdi <hmdy7486@gmail.com>
2024-08-28 06:27:00 +0300

    Add PaymentMethodItem Widget for Checkout UI

---
+++ lib/features/checkout/presentation/pages/payment_details_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:payment_getaways/core/widgets/cutom_app_bar.dart';
+import 'package:payment_getaways/features/checkout/presentation/widgets/payment_details_view_body.dart';
+
+class PaymentDetailsView extends StatelessWidget {
+  const PaymentDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(
+        title: 'Payment Details',
+      ),
+      body: const PaymentDetailsViewBody(),
+    );
+  }
+}
